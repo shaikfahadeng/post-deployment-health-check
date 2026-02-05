dc88785888645f580b3ca77b5b1cb3e221fbf731
shaikfahadeng <141911097+shaikfahadeng@users.noreply.github.com>
2026-02-05 19:02:19 +0530

    Update health_check.py

--- scripts/health_check.py
+++ scripts/health_check.py
@@ -6,7 +6,7 @@ print("Post-deployment health check started")
 url = "https://www.google.com"
 
 try:
-    urllib.request.urlopen(url, timeout=5)
+    urllib.request.urlopen(url, timeout=70)
     print("Application is healthy")
     sys.exit(0)   # success
 except:
