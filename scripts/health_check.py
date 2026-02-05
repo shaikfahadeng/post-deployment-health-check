61cb83f9ba13e21efd1b7fe392c043f89f15ddb5
shaikfahadeng <141911097+shaikfahadeng@users.noreply.github.com>
2026-02-05 18:10:58 +0530

    Fail script when health check fails

--- scripts/health_check.py
+++ scripts/health_check.py
@@ -1,4 +1,5 @@
 import urllib.request
+import sys
 
 print("Post-deployment health check started")
 
@@ -7,5 +8,7 @@ url = "https://www.google.com"
 try:
     urllib.request.urlopen(url, timeout=5)
     print("Application is healthy")
+    sys.exit(0)   # success
 except:
     print("Application is unhealthy")
+    sys.exit(1)   # failure
